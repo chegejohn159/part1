9bd28cc5ba1b7d7b750f4ad0e4ce62d582f5df4e
kamauchege159 <kamauchege159@gmail.com>
2020-08-03 23:09:13 +0300

    initial commit

---
+++ static/js/app.js
@@ -0,0 +1,40 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+function generateTable(table, data) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (key in element) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      cell.appendChild(text);
+    }
+  }
+}
+
+let table = document.querySelector("#ufo-table");
+generateTable(table, tableData);
+
+function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("datetime");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("ufo-table");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
